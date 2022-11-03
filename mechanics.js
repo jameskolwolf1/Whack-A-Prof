6b1fed85fb4671a29d2449757d5ed594b093c070
alexapineda0904 <77034186+alexapineda0904@users.noreply.github.com>
2022-11-03 01:47:30 -0400

    Add files via upload
    -Alexa Pineda- 
    
    I revamped the code to make it more understandable and add a list of things backbone needs to do

--- mechanics.js
+++ mechanics.js
@@ -1,30 +1,3 @@
 const holesdiv  = document.querySelector(".holes");
 const score = document.querySelector(".score");
 const time = document.querySelector(".time");
-
-
-
-for(let i = 1; i <= 9;i++){
-    let hole = document.createElement("div")
-    hole.classList.add("hole")
-    holesdiv.appendChild(hole);
-
-    let student = document.createElement("img");
-    student.classList.add("student");
-    student.src = "./images/student.png";
-
-    let professor = document.createElement("img");
-    professor.classList.add("professor");
-    professor.src = "./images/professor.png";
-
-    let dean = document.createElement("img");
-    dean.classList.add("dean");
-    dean.src = "./images/dean.png";
-
-    let admin = document.createElement("img");
-    admin.classList.add("admin");
-    admin.src = "./images/admin.png";
-
-
-
-}
