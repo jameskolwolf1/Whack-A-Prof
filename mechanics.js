dc08defca98a4d320418abef556267d2bc0ef256
alexapineda0904 <77034186+alexapineda0904@users.noreply.github.com>
2022-11-04 20:11:05 -0400

    Add files via upload
    *Added the Student Sprite
    *Added Cursor (blue square)
    *Added Score counter in js (doesn't seem to work well)

--- mechanics.js
+++ mechanics.js
@@ -1,3 +1,47 @@
-const holesdiv  = document.querySelector(".holes");
-const score = document.querySelector(".score");
-const time = document.querySelector(".time");
+const holes = [...document.querySelectorAll('.hole')]
+const scoreElement = document.querySelector('.score .span')
+const cursor = document.querySelector('.cursor')
+
+let score = 0
+let timer = null
+
+function run(){
+    const i = Math.floor(Math.random() * holes.length)
+    const hole = holes[i]
+    let timer = null
+
+    const img = document.createElement('img')
+    img.classList.add('students')
+    img.src = 'graphics/student/student.svg'
+
+    img.addEventListener('click', () => {
+        score -= 10
+        sound.play()
+        scoreElement.textContent = score
+        img.src = 'graphics/student/student_hit.svg'
+        clearTimeout(timer)
+        setTimeout(() => {
+            hole.removeChild(img)
+            run()
+        }, 500)
+    })
+
+    hole.appendChild(img)
+
+    timer = setTimeout(() => {
+        hole.removeChild(img)
+        run()
+    }, 2000)
+}
+run()
+
+window.addEventListener('mousemove', e => {
+    cursor.style.top = e.pageY + 'px'
+    cursor.style.left = e.pageX + 'px'
+})
+window.addEventListener('mousedown', () => {
+    cursor.classList.add('active')
+})
+window.addEventListener('mouseup', () => {
+    cursor.classList.remove('active')
+})
