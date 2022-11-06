e113f22c776151bcdef65ff75b0be9a8f5f94855
jameskolwolf1 <90884289+jameskolwolf1@users.noreply.github.com>
2022-11-05 22:41:11 -0400

    I adding a timer, start buttonm, and game over (will in the works) - James-LT

--- mechanics.js
+++ mechanics.js
@@ -1,9 +1,33 @@
 const holes = [...document.querySelectorAll('.hole')]
 const scoreElement = document.querySelector('.score .span')
 const cursor = document.querySelector('.cursor')
+const startTime = 1;
 
 let score = 0
 let timer = null
+
+const count = document.getElementById("time")
+
+//The event will start when the start button is clicked------------------------------------
+start.addEventListener('click', function()){
+
+	setInterval(countDown, 1000)
+	function countDown(){
+
+		const minutes = Math.Floor(timer/60)
+		let seconds = timer % 60;
+
+		seconds = seconds < 10 ? '0' + seconds : seconds
+		count.innerHTML = minutes + " : " + seconds
+		if(timer > 0){
+
+			timer--
+		}
+	}
+
+}
+//----------------------------------------------------------------------------------------
+
 
 function run(){
     const i = Math.floor(Math.random() * holes.length)
@@ -33,8 +57,31 @@ function run(){
         run()
     }, 2000)
 }
-run()
+// The start bnutton wioll start the ghame when clicked ----------------------------------------
+function StartGame(){
 
+
+	let startDiv = document.getElementById("Start");
+	let gameCanvas = document.getElementById("canvas");
+	let gameover = document.getElementByID("game-over");
+
+	startDiv.style.display = "none";
+	gameCanvas.style.display = "none";
+	gameover.style.display = "none";
+
+	run();
+}
+function gameOver(){
+
+	let startDiv = document.getElementById("start");
+	let gameCanvas = document.getElementById("canvas");
+	let gameover = document.getElementById("game-over");
+
+	startDiv.style.display = "none";
+	gameCanvas.style.display = "none";
+	gameover.style.display = "block";
+}
+//--------------------------------------------------------------------------------------------
 window.addEventListener('mousemove', e => {
     cursor.style.top = e.pageY + 'px'
     cursor.style.left = e.pageX + 'px'
